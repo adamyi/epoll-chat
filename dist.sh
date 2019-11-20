5e8bb04a6ea9109918bebe53b52f6b26f5e9c167
Adam Yi <z5231521@cse.unsw.edu.au>
2019-11-21 01:24:11 +1100

    fix submission script

--- dist.sh
+++ dist.sh
@@ -1,4 +1,7 @@
+rm proto/*.c proto/*.h
+bazel build //achelper:ac_protoc
+bazel-bin/achelper/ac_protoc request.proto response.proto proto
 rm report.pdf
 wget -O report.pdf "https://docs.google.com/a/adamyi.com/document/export?format=pdf&id=17CRc4Ejcu5sEyTOdjDHsXUK0wlssklqABTgneGW0BlE"
-tar cvf assign.tar report.pdf WORKSPACE BUILD.bazel achelper/BUILD.bazel achelper/*.c achelper/*.h lib/ clientlib/ serverlib/ client.c server.c
+tar cvf assign.tar report.pdf WORKSPACE BUILD.bazel Makefile achelper/BUILD.bazel achelper/*.c achelper/*.h lib/ clientlib/ serverlib/ proto/ client.c server.c *.proto
 give cs3331 assign assign.tar
